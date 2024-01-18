dd6db78a347f4c2a7a2f40c34d2ac57273c116a8
Hedgehog99 <ntt.tram.tran@hotmail.com>
2024-01-18 18:35:44 +1100

    update for remote work

---
+++ book/BookStore.hpp
@@ -0,0 +1,13 @@
+#ifndef     BOOKSTORE_HPP
+#define     BOOKSTORE_HPP
+
+class BookStore;
+class BookStore{
+    public:
+        std::string storeName;
+        std::string owner;
+
+        
+};
+
+#endif
