3fa3e26ed6a14216a09928f7a545819441b5e3c2
Tram Tran <ntt.tram.tran@hotmail.com>
2024-01-19 16:11:42 +1100

    BookStore updates progress

--- book/BookStore.hpp
+++ book/BookStore.hpp
@@ -1,12 +1,28 @@
 #ifndef     BOOKSTORE_HPP
 #define     BOOKSTORE_HPP
 
+#include <iostream>
+#include "Inventory.hpp"
+#include "Book.hpp"
+
 class BookStore;
 class BookStore{
     public:
         std::string storeName;
         std::string owner;
 
+        std::map<std::string, Inventory*> inventories;
+
+        BookStore();
+        ~BookStore();
+
+        BookStore(std::string storeName, std::string owner);
+
+        void buyBook(int bookID, int quantity);
+        void sellBook(int bookID, int quantity);
+
+        void addInventory(Inventory* invent);
+        void removeInventory(Inventory* invent);
         
 };
 
